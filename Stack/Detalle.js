353efd743cbbbe1f96b2b8de5bd45f5fa1c727b1
Mijael Rocha <josemijaelrochavera666@gmail.com>
2022-08-28 19:57:21 -0400

    haciendo las caracteristicas del auto

--- Stack/Detalle.js
+++ Stack/Detalle.js
@@ -44,20 +44,27 @@ function Detalle() {
                     Datos de vehículo
                 </Text>
                 <View
-                
+                    style={styles.ContenedorDos}
+
                 >
                     <View
-                        // style={styles.}
+                        style={styles.contenedorCaracteristicas}
                     >
-                        <Text>
+                        <Text
+                            style={styles.OT}
+                        >
                             OT 124142
                         </Text>
 
-                        <Text>
+                        <Text
+                            style={styles.marcaauto}
+                        >
                             BMWM4 2020
                         </Text>
 
-                        <Text>
+                        <Text
+                            style={styles.placaVIN}
+                        >
                             Placa/VIN
                         </Text>
                     </View>
@@ -65,10 +72,123 @@ function Detalle() {
                     <View
                         style={styles.contenedorAuto}
                     >
-                        <Image 
+                        <Image
                             style={styles.logoAuto}
                             source={require("../assets/logoAuto.png")}
                         />
+                    </View>
+                </View>
+
+                <View
+                    style={styles.contenedorImagenGrande}
+                >
+                    <Image 
+                        style={styles.imagenAutoGrande}
+                        source={require("../assets/autos/auto_2.png")}
+                    />
+                </View>
+
+                <View
+                    style={styles.ContenedorDos}
+                >
+                    <View
+                        style={styles.contenedorBlanco}
+                    >
+                        <Text
+                        style={styles.textoTresNegro}
+                        >
+                            Marca
+                        </Text>
+
+                        <Text
+                            style={styles.contenedorTresClaro}
+                        >
+                            Mercedes Benz
+                        </Text>
+                    </View>
+
+                    <View
+                        style={styles.contenedorBlanco}
+                    >
+                        <Text
+                        style={styles.textoTresNegro}
+                        >
+                            Modelo
+                        </Text>
+
+                        <Text
+                            style={styles.contenedorTresClaro}
+                        >
+                            Modelo
+                        </Text>
+                    </View>
+
+                    <View
+                        style={styles.contenedorBlanco}
+                    >
+                        <Text
+                        style={styles.textoTresNegro}
+                        >
+                            Versión
+                        </Text>
+
+                        <Text
+                            style={styles.contenedorTresClaro}
+                        >
+                            Versión
+                        </Text>
+                    </View>
+                </View>
+
+                <View
+                    style={styles.ContenedorDos}
+                >
+                    <View
+                        style={styles.contenedorBlanco}
+                    >
+                        <Text
+                        style={styles.textoTresNegro}
+                        >
+                            Marca
+                        </Text>
+
+                        <Text
+                            style={styles.contenedorTresClaro}
+                        >
+                            Mercedes Benz
+                        </Text>
+                    </View>
+
+                    <View
+                        style={styles.contenedorBlanco}
+                    >
+                        <Text
+                        style={styles.textoTresNegro}
+                        >
+                            Modelo
+                        </Text>
+
+                        <Text
+                            style={styles.contenedorTresClaro}
+                        >
+                            Modelo
+                        </Text>
+                    </View>
+
+                    <View
+                        style={styles.contenedorBlanco}
+                    >
+                        <Text
+                        style={styles.textoTresNegro}
+                        >
+                            Versión
+                        </Text>
+
+                        <Text
+                            style={styles.contenedorTresClaro}
+                        >
+                            Versión
+                        </Text>
                     </View>
                 </View>
             </View>
@@ -88,15 +208,15 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
-    atras:{
-        marginRight:150
+    atras: {
+        marginRight: 150
     },
     campana: {
         width: 28,
         height: 35,
         marginLeft: 150,
     },
-    contenedorDatos:{
+    contenedorDatos: {
         backgroundColor: '#EAEBEC',
         borderRadius: 40,
         width: 340,
@@ -104,21 +224,75 @@ const styles = StyleSheet.create({
         marginTop: 25
     },
     logoAuto: {
-        width: 40,
-        height: 40
+        width: 48,
+        height: 46,
     },
     contenedorAuto: {
         backgroundColor: '#FDFDFD',
-        width: 50,
-        height:50,
+        width: 62,
+        height:62,
+        borderRadius: 100,
+        marginTop: 28,
+        marginRight: 28,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     TituloAuto: {
-        fontSize: 25,
+        fontSize: 22,
         marginTop: 28,
         marginLeft: 28,
         color: '#000000',
         fontWeight: '600'
     },
-})
+    ContenedorDos: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    OT:{
+        fontSize: 28,
+        fontWeight: '600'
+    },
+    marcaauto: {
+        fontSize: 18,
+        fontWeight: '500',
+    },
+    placaVIN: {
+        color: '#B6B6B6',
+        fontSize: 12,
+        fontWeight: '300'
+    },
+    contenedorCaracteristicas: {
+        marginTop: 28,
+        marginLeft: 28
+    },
+    imagenAutoGrande: {
+        width: 290,
+        height: 195,
+
+    },
+    contenedorImagenGrande:{
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textoTresNegro: {
+        fontSize: 14,
+        fontWeight: '500',
+        marginLeft:5
+    },
+    contenedorTresClaro: {
+        fontSize: 14,
+        fontWeight: '500',
+        color: '#B6B6B6',
+        marginLeft:5,
+    },
+    contenedorBlanco: {
+        backgroundColor: '#FFFFFF',
+        width: 105,
+        height:55,
+        borderRadius: 12,
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+});
 
 export default Detalle;
